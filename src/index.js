daf27612a1be11d028d02e7e7b990bbdd49321ca
joohee-kim <joohee.kim15@navercorp.com>
2020-09-04 21:54:40 +0900

    index 파일 포맷정리

--- src/index.js
+++ src/index.js
@@ -1,84 +1,84 @@
+const eventDateArea = document.querySelector("#event_area");
+const remainDateArea = document.querySelector("#remain_area");
 
-	const eventDateArea = document.querySelector("#event_area");
-	const remainDateArea = document.querySelector("#remain_area");
+const MIL_SECOND_UNIT = 1000;
+const SECOND_UNIT = 60;
+const MINUTE_UNIT = 60;
+const HOUR_UNIT = 24;
 
-	const MIL_SECOND_UNIT = 1000;
-	const SECOND_UNIT = 60;
-	const MINUTE_UNIT = 60;
-	const HOUR_UNIT = 24;
+const DAY_TO_SEC = HOUR_UNIT * MINUTE_UNIT * SECOND_UNIT;
+const HOUR_TO_SEC = MINUTE_UNIT * SECOND_UNIT;
+const MINUTE_TO_SEC = MINUTE_UNIT;
 
-	const DAY_TO_SEC = HOUR_UNIT * MINUTE_UNIT * SECOND_UNIT;
-	const HOUR_TO_SEC = MINUTE_UNIT * SECOND_UNIT;
-	const MINUTE_TO_SEC = MINUTE_UNIT;
+const getDateTime = () => {
+	debugger;
+	const timeFromQuery = window.location.search;
+	const parseExp = /(?<=[time\=])\d+/g
+	const matchTarget = timeFromQuery.match(parseExp) || [];
+	const dateTime = new Date(parseInt(matchTarget[0], 10));
 
-	const getDateTime = () => {
-		const timeFromQuery = window.location.search;
-		const parseExp = /(?<=[time\=])\d+/g
-		const matchTarget = timeFromQuery.match(parseExp) || [];
-		const dateTime = new Date(parseInt(matchTarget[0], 10));
-
-		if(isNaN(dateTime.getTime())) {
-			return null;
-		}
-
-		return dateTime;
+	if(isNaN(dateTime.getTime())) {
+		return null;
 	}
 
-
-	const parseTimeStamp = (milsec) => {
-		const remainSecond = parseInt(Math.abs(milsec)/MIL_SECOND_UNIT, 10);
-		
-		const day = parseInt(remainSecond/DAY_TO_SEC, 10);
-		const hour = parseInt((remainSecond-day*DAY_TO_SEC)/HOUR_TO_SEC, 10);
-		const min = parseInt((remainSecond-day*DAY_TO_SEC-hour*HOUR_TO_SEC)/MINUTE_TO_SEC, 10);
-		const sec = remainSecond-day*DAY_TO_SEC-hour*HOUR_TO_SEC-min*MINUTE_TO_SEC;
+	return dateTime;
+}
 
 
-		return {
-			day: day,
-			hour: hour,
-			minute: min,
-			second: sec
-		}
+const parseTimeStamp = (milsec) => {
+	const remainSecond = parseInt(Math.abs(milsec)/MIL_SECOND_UNIT, 10);
+	
+	const day = parseInt(remainSecond/DAY_TO_SEC, 10);
+	const hour = parseInt((remainSecond-day*DAY_TO_SEC)/HOUR_TO_SEC, 10);
+	const min = parseInt((remainSecond-day*DAY_TO_SEC-hour*HOUR_TO_SEC)/MINUTE_TO_SEC, 10);
+	const sec = remainSecond-day*DAY_TO_SEC-hour*HOUR_TO_SEC-min*MINUTE_TO_SEC;
+
+
+	return {
+		day: day,
+		hour: hour,
+		minute: min,
+		second: sec
 	}
+}
 
-	const displayEventDate = (eventDate) => {
-		eventDateArea.innerHTML = `<span>${eventDate} 날짜 까지</span>`;
-	}
+const displayEventDate = (eventDate) => {
+	eventDateArea.innerHTML = `<span>${eventDate} 날짜 까지</span>`;
+}
 
 
-	const displayRemainTime = (eventDate) => {
-		const callFrame = () => {
-			const remainTime = Date.now() - eventDate.getTime();
+const displayRemainTime = (eventDate) => {
+	const callFrame = () => {
+		const remainTime = Date.now() - eventDate.getTime();
 
-			if(remainTime > 0) {
-				// 지나간 시간
-				return ;
-			}
-			const {day, hour, minute, second} = parseTimeStamp(remainTime);
-
-			remainDateArea.innerHTML = `<span>D-${day}:${hour}:${minute}:${second}</span>`;	
-
-			requestAnimationFrame(callFrame);
+		if(remainTime > 0) {
+			// 지나간 시간
+			return ;
 		}
+		const {day, hour, minute, second} = parseTimeStamp(remainTime);
+
+		remainDateArea.innerHTML = `<span>D-${day}:${hour}:${minute}:${second}</span>`;	
 
 		requestAnimationFrame(callFrame);
 	}
 
-	const init = () => {
-		// 1. 쿼리 확인
-		// ex) date=1601859600000 
-		let eventDate = getDateTime();
+	requestAnimationFrame(callFrame);
+}
 
-		if(!eventDate) {
-			// 1.5. 캘린더 보여주기 => 날짜 선택하기 => 쿼리 바꾸기
-		}
+const init = () => {
+	// 1. 쿼리 확인
+	// ex) date=1601859600000 
+	let eventDate = getDateTime();
 
-		// 2. 화면에 현재 시간 및 남은 시간 표시하기
-		displayEventDate(eventDate);
-		displayRemainTime(eventDate);
+	if(!eventDate) {
+		// 1.5. 캘린더 보여주기 => 날짜 선택하기 => 쿼리 바꾸기
 	}
 
-	window.addEventListener("DOMContentLoaded", function() {
-		init();
-	});
+	// 2. 화면에 현재 시간 및 남은 시간 표시하기
+	displayEventDate(eventDate);
+	displayRemainTime(eventDate);
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+	init();
+});
